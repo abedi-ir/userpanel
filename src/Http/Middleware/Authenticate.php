316a4214e0b90d91fe400ea5bff74cac2240ee30
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2021-07-05 14:39:14 +0430

    Improve phpdocs

--- src/Http/Middleware/Authenticate.php
+++ src/Http/Middleware/Authenticate.php
@@ -9,32 +9,13 @@ use Illuminate\Contracts\Auth\Factory as Auth;
 
 class Authenticate
 {
-    /**
-     * The authentication guard factory instance.
-     *
-     * @var Auth
-     */
-    protected $auth;
+    protected Auth $auth;
 
-    /**
-     * Create a new middleware instance.
-     *
-     * @param  Auth  $auth
-     * @return void
-     */
     public function __construct(Auth $auth)
     {
         $this->auth = $auth;
     }
 
-    /**
-     * Handle an incoming request.
-     *
-     * @param  Request      $request
-     * @param  Closure      $next
-     * @param  string|null  $guard
-     * @return mixed
-     */
     public function handle(Request $request, Closure $next, ?string $guard = null)
     {
         if (!$request->user()) {
